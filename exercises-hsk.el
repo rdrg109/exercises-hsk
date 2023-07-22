0f4053ca04543f6fc7f1ce0f18164fa6d7a34b8c
Rodrigo Morales <moralesrodrigo1100@gmail.com>
2023-07-22 09:42:43 -0500

    add: function for exporting "Exercises/2"

--- exercises-hsk.el
+++ exercises-hsk.el
@@ -44,4 +44,42 @@
    :outline '("练习" "1" "选择")
    :testfn '<))
 
+(cl-defun hsk-5-textbook-exercises-part-2-export (&key filename-input filename-output)
+  (with-current-buffer (find-file-noselect filename-output)
+    (let* ((outline '("练习" "2"))
+           (deck
+            (exercises-hsk-build-deck-name-from-file-and-outline
+             filename-input outline))
+           (notetype "4d1324a6-7ce2-4290-a7f7-7449d6739058")
+           (data (exercises-get-subtree-as-alist
+                  :filename filename-input
+                  :outline outline))
+           (exercises (cdr data)))
+      (insert
+       (string-join
+        (cl-loop
+         for exercise in exercises
+         collect
+         (string-join
+          (list
+           deck
+           notetype
+           (alist-get "id" exercise nil nil 'equal)
+           (alist-get "句子填空" (cdr exercise) nil nil 'equal)
+           (alist-get "A" (alist-get "选择" (cdr exercise) nil nil 'equal) nil nil 'equal)
+           (alist-get "B" (alist-get "选择" (cdr exercise) nil nil 'equal) nil nil 'equal)
+           (alist-get "答案" (cdr exercise) nil nil 'equal))
+          "	"))
+        "\n")
+       "\n"))))
 
+(cl-defun exercises-hsk-build-deck-name-from-file-and-outline (file outline)
+  (concat
+   (replace-regexp-in-string
+    "/"
+    "::"
+    (file-relative-name
+     (replace-regexp-in-string "\\.org\\'" "" file)
+     exercises-hsk-directory))
+   "::"
+   (string-join outline "::")))
