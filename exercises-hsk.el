2de458e50456ea802050f3bc28369c94cbe5c154
Rodrigo Morales <moralesrodrigo1100@gmail.com>
2023-08-17 12:30:56 -0500

    edit: exercises-hsk-export-content-with-audio-multiple-choice-exercises
    Support subtrees containing a single exercise and subtrees containing
    multiple exercises.

--- exercises-hsk.el
+++ exercises-hsk.el
@@ -6,9 +6,9 @@
   '(("7304a4a2-efe6-4d8e-96dc-e419347c7a56" . exercises-hsk-export-content-with-audio-multiple-choice-exercises))
   "")
 
-(cl-defun exercises-hsk-export-content-with-audio-multiple-choice-exercises (&key
-                                                                             filename-source
-                                                                             outline)
+(cl-defun exercises-hsk-export-content-with-audio-multiple-choice-exercises
+    (&key filename-source
+          outline)
   (let* ((deck (exercises-anki-build-deck-name-from-file-and-outline
                 :dir exercises-hsk-root-dir
                 :filename filename-source
@@ -20,112 +20,85 @@
                    (catch 'found
                      (cl-loop
                       for item in (alist-get 'entries data)
-                      when (member (alist-get 'headline item) '("对话" "课文"))
+                      when (member (alist-get 'headline item) '("对话" "课文" "段话"))
                       do (throw 'found (alist-get 'content item))))))
          (content-audio (catch 'found
                           (cl-loop
                            for item in (alist-get 'entries data)
-                           when (member (alist-get 'headline item) '("对话" "课文"))
+                           when (member (alist-get 'headline item) '("对话" "课文" "段话"))
                            do (cl-loop
                                for item-2 in (alist-get 'entries item)
                                when (equal (alist-get 'headline item-2) "音频")
-                               do (throw 'found (alist-get 'content item-2))))))
-         (exercises-entries (catch 'found
-                              (cl-loop
-                               for item in (alist-get 'entries data)
-                               when (equal (alist-get 'headline item) "题目")
-                               do (throw 'found (alist-get 'entries item)))))
-         (exercises (cl-loop
-                     for exercise in exercises-entries
-                     collect `((id . ,(alist-get "id" (alist-get 'properties exercise) nil nil 'equal))
-                               (headline . ,(alist-get 'headline exercise))
-                               (question . ,(catch 'found
-                                              (cl-loop
-                                               for entry in (alist-get 'entries exercise)
-                                               when (equal (alist-get 'headline entry) "问题")
-                                               do (throw 'found (alist-get 'content entry)))))
-                               (question-audio . ,(catch 'found
-                                                    (cl-loop
-                                                     for item in (alist-get 'entries exercise)
-                                                     when (equal (alist-get 'headline item) "问题")
-                                                     do (cl-loop
-                                                         for item-2 in (alist-get 'entries item)
-                                                         when (equal (alist-get 'headline item-2) "音频")
-                                                         do (throw 'found (alist-get 'content item-2))))))
-                               (alternatives . ,(catch 'value
-                                                  (cl-loop
-                                                   for entry in (alist-get 'entries exercise)
-                                                   when (equal (alist-get 'headline entry) "选择")
-                                                   do (throw 'value
-                                                             (cl-loop
-                                                              for alternative in (alist-get 'entries entry)
-                                                              collect (cons (alist-get 'headline alternative)
-                                                                            (alist-get 'content alternative)))))))
-                               (answer . ,(catch 'found
-                                            (cl-loop
-                                             for entry in (alist-get 'entries exercise)
-                                             when (equal (alist-get 'headline entry) "答案")
-                                             do (throw 'found (alist-get 'content entry)))))))))
+                               do (throw 'found (alist-get 'content item-2)))))))
     (with-current-buffer (find-file-noselect exercises-hsk-export-exported-file)
-      (insert
-       (string-join
-        (exercises-flatten
-         (list
-          deck
-          "audio-multiple-choice-exercise"
-          "listening multiple-exercises"
-          (alist-get "id" (alist-get 'properties data) nil nil 'equal)
-          content
-          content-audio
-          (cl-loop
-           for exercise in exercises
-           collect (mapcar 'cdr exercise))))
-        "	")
-       "\n")
-      (cl-loop
-       for exercise in exercises
-       do (insert
-           (string-join
-            (exercises-flatten
-             (list
-              deck
-              "audio-multiple-choice-exercise"
-              "listening single-exercise"
-              (alist-get 'id exercise)
-              content
-              content-audio
-              (mapcar 'cdr exercise)))
-            "	")
-           "\n"))
-      (insert
-       (string-join
-        (exercises-flatten
-         (list
-          deck
-          "text-multiple-choice-exercise"
-          "reading multiple-exercises"
-          (alist-get "id" (alist-get 'properties data) nil nil 'equal)
-          content
-          (cl-loop
-           for exercise in exercises
-           collect (mapcar 'cdr (delq (assoc 'question-audio exercise) exercise)))
-          ))
-        "	")
-       "\n")
-      (cl-loop
-       for exercise in exercises
-       do (insert
-           (string-join
-            (exercises-flatten
-             (list
-              deck
-              "text-multiple-choice-exercise"
-              "reading single-exercise"
-              (alist-get 'id exercise)
-              content
-              (mapcar 'cdr (delq (assoc 'question-audio exercise) exercise))))
-            "	")
-           "\n")))))
+      (if (catch 'found
+            (cl-loop
+             for item in (alist-get 'entries data)
+             when (equal (alist-get 'headline item) "题目")
+             do (throw 'found (alist-get 'entries item))))
+          ;; Multiple exercises
+          (let ((exercises (exercises-extract-data-multiple-exercises :data data)))
+            (exercises-anki-insert-note
+             :deck deck
+             :note-type "audio-multiple-choice-exercise"
+             :tags '("listening"
+                     "multiple-exercises")
+             :fields (list (alist-get "id" (alist-get 'properties data) nil nil 'equal)
+                           content
+                           content-audio
+                           (cl-loop
+                            for exercise in exercises
+                            collect (mapcar 'cdr exercise))))
+            (cl-loop
+             for exercise in exercises
+             do (exercises-anki-insert-note
+                 :deck deck
+                 :note-type "audio-multiple-choice-exercise"
+                 :tags '("listening"
+                         "single-exercise")
+                 :fields (list (alist-get 'id exercise)
+                               content
+                               content-audio
+                               (mapcar 'cdr exercise))))
+            (exercises-anki-insert-note
+             :deck deck
+             :note-type "text-multiple-choice-exercise"
+             :tags '("reading"
+                     "multiple-exercises")
+             :fields (list (alist-get "id" (alist-get 'properties data) nil nil 'equal)
+                           content
+                           (cl-loop
+                            for exercise in exercises
+                            collect (mapcar 'cdr (delq (assoc 'question-audio exercise) exercise)))))
+            (cl-loop
+             for exercise in exercises
+             do (exercises-anki-insert-note
+                 :deck deck
+                 :note-type "text-multiple-choice-exercise"
+                 :tags '("reading"
+                         "single-exercise")
+                 :fields (list (alist-get 'id exercise)
+                               content
+                               (mapcar 'cdr (delq (assoc 'question-audio exercise) exercise))))))
+        ;; Single exercise
+        (let ((exercise (exercises-extract-data-single-exercise :data data)))
+          (exercises-anki-insert-note
+           :deck deck
+           :note-type "text-multiple-choice-exercise"
+           :tags '("reading"
+                   "single-exercise")
+           :fields (list (alist-get 'id exercise)
+                         content
+                         (mapcar 'cdr (delq (assoc 'question-audio exercise) exercise))))
+          (exercises-anki-insert-note
+           :deck deck
+           :note-type "audio-multiple-choice-exercise"
+           :tags '("listening"
+                   "single-exercise")
+           :fields (list (alist-get 'id exercise)
+                         content
+                         content-audio
+                         (mapcar 'cdr exercise))))))))
 
 (cl-defun exercises-hsk-export-children-content-with-single-question (&key
                                                                       filename-source
